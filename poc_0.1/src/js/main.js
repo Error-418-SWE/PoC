4cecd1fb8150d2605b0ad1adcb6fefe28a61e345
SilvioNardo <126958735+SilvioNardo@users.noreply.github.com>
2023-11-27 19:17:16 +0100

    tolte cose in più

--- poc_0.1/src/js/main.js
+++ poc_0.1/src/js/main.js
@@ -28,73 +28,9 @@ scene.add(gridHelper);
 //tools gui
 const gui = new dat.GUI();
 
-/*
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-let objects = []  
-const box = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial( { color: 0xBE7363 } ));
-const edges = new THREE.EdgesGeometry( box.geometry ); 
-const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0x000000 } ) ); 
-box.add( line );
-
-var obj = { 
-    'Crea scatola':function(){ 
-        //box
-        const clonedBox = box.clone();
-        scene.add( clonedBox );
-        clonedBox.position.set(getRandomInt(40) - 20 + 0.5,0.5,getRandomInt(40) - 20 + 0.5);
-        objects.push(clonedBox);
-    },
-    'Elimina scatole':function(){ 
-        objects.forEach(function(object){
-            scene.remove(object);
-        })
-        objects = [];
-    }
-};
-
-
-gui.add(obj,'Crea scatola');
-gui.add(obj,'Elimina scatole');
-
-
-const raycaster = new THREE.Raycaster();
-let intersects;
-
-const mousePosition = new THREE.Vector2();
-const normalizedMousePosition = new THREE.Vector2();
-
-window.addEventListener('mousemove',function(e){
-    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
-    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
-    normalizedMousePosition.x = (mousePosition.x).floor().addScalar(0.5);
-    normalizedMousePosition.y = (mousePosition.y).floor().addScalar(0.5);
-});
-
-
-window.addEventListener('mousedown',function(){
-    const objectExist = objects.find(function(object){
-        return ((object.position.x === normalizedMousePosition.x) && (object.position.z === normalizedMousePosition.z));
-    });
-    if(!objectExist){
-        intersects = raycaster.intersectObjects(scene.children);
-        intersects.forEach(function(intersect){
-            if(intersect.object.name === 'ground'){
-                const clonedBox = box.clone();
-                scene.add( clonedBox );
-                objects.push(clonedBox);
-                clonedBox.position.copy(intersect.point).floor().addScalar(0.5);
-            }
-        });
-    }
-});
-
-*/
 
 function animate(time) {
     renderer.render( scene, camera );
-
 }
 
 renderer.setAnimationLoop(animate);
