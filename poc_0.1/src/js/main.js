28cb0ee8f7bcd27f303bcb17ec89571b481b9643
SilvioNardo <126958735+SilvioNardo@users.noreply.github.com>
2023-11-28 19:57:29 +0100

    close #2 creazione scaffale sul cursore del mouse rimovibile da tastiera

--- poc_0.1/src/js/main.js
+++ poc_0.1/src/js/main.js
@@ -28,6 +28,70 @@ scene.add(gridHelper);
 //tools gui
 const gui = new dat.GUI();
 
+//mesh scaffale
+const box = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial( { color: 0xBE7363 } ));
+
+let isScaffaleDaPosizionare = false;
+let ancoredObject;
+const mousePosition = new THREE.Vector2();
+const intersectionPoint = new THREE.Vector3();
+const planeNormal = new THREE.Vector3();
+const plane2 = new THREE.Plane();
+const raycaster = new THREE.Raycaster();
+
+function getMouse3DPos(){
+    planeNormal.copy(camera.position).normalize();
+    plane2.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+    raycaster.setFromCamera(mousePosition, camera);
+    raycaster.ray.intersectPlane(plane2, intersectionPoint);
+    return intersectionPoint;
+}
+
+
+var obj = { 
+    'Crea scaffale':function(){ 
+        const nuovoScaffale = box.clone();
+        scene.add( nuovoScaffale );
+        isScaffaleDaPosizionare = true;
+        ancoredObject = nuovoScaffale;
+        nuovoScaffale.position.copy(getMouse3DPos());
+    }
+}
+
+gui.add(obj,'Crea scaffale');
+
+window.addEventListener('mousemove',function(e){
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    if (isScaffaleDaPosizionare){
+        
+        ancoredObject.position.copy(getMouse3DPos());
+    }  
+});
+
+function removeMouseAnchor(){
+    isScaffaleDaPosizionare = false;
+    scene.remove(ancoredObject);
+}
+
+
+document.addEventListener('keydown', function(event) {
+    if (isScaffaleDaPosizionare){
+        if (event.key === "Delete" || event.key === "Backspace" || event.key === "Escape" || event.button === 2)  {
+            removeMouseAnchor();
+        }
+    }
+});
+
+
+document.addEventListener('mousedown', function(event) {
+    if (isScaffaleDaPosizionare){
+        if ( event.button === 2)  {
+            removeMouseAnchor();
+        }
+    }
+});
+
 
 function animate(time) {
     renderer.render( scene, camera );
