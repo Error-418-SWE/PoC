6eb7fe5eb2280057b96c4675ae1f03ee43f1b7d3
SilvioNardo <126958735+SilvioNardo@users.noreply.github.com>
2023-12-02 23:47:28 +0100

    aggiunta features corrispondenti alle issue 4, 11, 12

--- poc_0.1/src/js/main.js
+++ poc_0.1/src/js/main.js
@@ -16,22 +16,42 @@ camera.position.set(-10,30,30);
 const orbit = new OrbitControls(camera, renderer.domElement); //serve per muovere la visuale col mouse
 orbit.update();
 
+
+//tools gui
+// Aggiungi campi di input per larghezza, altezza e profondità nella GUI
+const boxDimensions = { larghezza: 2, altezza: 2, profondita: 2 };
+const gui = new dat.GUI();
+const folder = gui.addFolder('Dimensioni Scaffale');
+folder.add(boxDimensions, 'larghezza', 1, 10).step(1).name('Larghezza');
+folder.add(boxDimensions, 'altezza', 1, 10).step(1).name('Altezza');
+folder.add(boxDimensions, 'profondita', 1, 10).step(1).name('Profondità');
+folder.open();
+
+// dimensione della cella della griglia del piano
+const gridOptions = {
+    GridSize: 1
+};
+
 //piano 
-const plane = new THREE.Mesh( new THREE.PlaneGeometry(40, 40), new THREE.MeshBasicMaterial( { color: 0xFFFFFf, side: THREE.DoubleSide } ) );
+const latoPiano = 40;
+const plane = new THREE.Mesh( new THREE.PlaneGeometry(latoPiano, latoPiano), new THREE.MeshBasicMaterial( { color: 0xFFFFFf, side: THREE.DoubleSide } ) );
 scene.add( plane );
 plane.name = 'ground';
 plane.rotation.x = -0.5 * Math.PI;
 
-const gridHelper = new THREE.GridHelper(40,40);
-scene.add(gridHelper);
+//griglia
+let numberOfGridRows =  latoPiano / gridOptions.GridSize;
+let gridHelper = new THREE.GridHelper(latoPiano,numberOfGridRows);
+gridHelper.rotation.x = -0.5 * Math.PI;
+plane.add(gridHelper);
 
-//tools gui
-const gui = new dat.GUI();
 
 //mesh scaffale
+let ancorObjectColor = 0xBE7363; // colore del box scaffale
+let ancorObjectCollisionColor = 0xFF0000; // Colore del box ancorato in caso di collisione
 function creaScaffale(larghezza, altezza, profondita){
     const boxGeometry = new THREE.BoxGeometry(larghezza, altezza, profondita);
-    const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xBE7363 });
+    const boxMaterial = new THREE.MeshBasicMaterial({color: ancorObjectColor});
     const box = new THREE.Mesh( boxGeometry,boxMaterial);
     const edges = new THREE.EdgesGeometry( boxGeometry ); 
     const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 })); 
@@ -39,7 +59,7 @@ function creaScaffale(larghezza, altezza, profondita){
     return box;
 }
 
-// var
+// variabili per traking del mouse e il posizionamento dei box ancorati
 let isScaffaleDaPosizionare = false;
 let ancoredObject;
 const mousePosition = new THREE.Vector2();
@@ -47,15 +67,6 @@ const intersectionPoint = new THREE.Vector3();
 const planeNormal = new THREE.Vector3();
 const plane2 = new THREE.Plane();
 const raycaster = new THREE.Raycaster();
-
-
-// Aggiungi campi di input per larghezza, altezza e profondità nella GUI
-const boxDimensions = { larghezza: 1, altezza: 1, profondita: 1 };
-const folder = gui.addFolder('Dimensioni Scaffale');
-folder.add(boxDimensions, 'larghezza', 1, 10).step(1).name('Larghezza');
-folder.add(boxDimensions, 'altezza', 1, 10).step(1).name('Altezza');
-folder.add(boxDimensions, 'profondita', 1, 10).step(1).name('Profondità');
-folder.open();
 
 
 // funzione che ritorna una posizione nell'ambiente di lavoro (3d) data la posizione del mouse (2d)
@@ -67,9 +78,9 @@ function getMouse3DPos(){
     return intersectionPoint;
 }
 
+//bottone della gui per creare lo scaffale
 var obj = { 
     'Crea scaffale':function(){ 
-        //const nuovoScaffale = box.clone();
         const nuovoScaffale = creaScaffale(boxDimensions.larghezza ,boxDimensions.altezza ,boxDimensions.profondita);
         scene.add( nuovoScaffale );
         isScaffaleDaPosizionare = true;
@@ -79,19 +90,80 @@ var obj = {
 }
 
 gui.add(obj,'Crea scaffale');
+let isGridOn = true;
+gui.add( gridOptions, 'GridSize', 0, 1.5).step(0.5).name('Grid Size').onChange(function(value){
+    if(value === 0){
+        plane.remove(gridHelper);
+        isGridOn = false;
+    }
+    else{
+        if(!isGridOn)isGridOn = true;
+        numberOfGridRows =  latoPiano / value;
+        gridHelper.divisions = numberOfGridRows;
+        plane.remove(gridHelper);
+        gridHelper = new THREE.GridHelper(latoPiano,numberOfGridRows);
+        plane.add(gridHelper);
+        gridHelper.rotation.x = -0.5 * Math.PI;
+    }
+});
+
+// Funzione per verificare collisioni con altri oggetti
+function checkCollision() {
+    const boundingBox = new THREE.Box3().setFromObject(ancoredObject); //serve perchè l'intersectsBox funziona su tipi box3
+    let collision = false;
+    scene.children.forEach((object) => {
+        if (object !== ancoredObject && object.name !== 'ground') {
+            const otherBoundingBox = new THREE.Box3().setFromObject(object); //serve perchè l'intersectsBox funziona su tipi box3
+            otherBoundingBox.expandByScalar(-0.01); // riduco un minimo il box se no due box in contatto li conta come in collisione
+            if (boundingBox.intersectsBox(otherBoundingBox)) { 
+                collision = true;
+                return collision;
+            }
+        }
+    });
+    return collision;
+}
+
 
 // funzione che posiziona ancoredObject alla posizione corrispondente nel piano se si passa con il mouse sopra ad esso 
 function hoverGround(intersects){
     let isIntersectionFound = false;
     intersects.forEach(function(intersect){
         if(intersect.object.name === 'ground'){
-        ancoredObject.position.copy(intersect.point).floor().addScalar(0.5);
-        ancoredObject.position.y = ancoredObject.geometry.parameters.height / 2 ;
-        isIntersectionFound = true;
+            if(isGridOn){
+                ancoredObject.position.copy(intersect.point).divideScalar(gridOptions.GridSize).floor().multiplyScalar(gridOptions.GridSize);
+                //correzione posizionamento lunghezza dispari
+                if((ancoredObject.geometry.parameters.width % 2) !== 0){
+                    ancoredObject.position.x -= (ancoredObject.geometry.parameters.width / 2) % gridOptions.GridSize;
+                }
+ 
+                //correzione posizionamento profondità dispari
+                if((ancoredObject.geometry.parameters.depth % 2) !== 0){
+                    ancoredObject.position.z -= (ancoredObject.geometry.parameters.depth / 2) % gridOptions.GridSize;
+                }
+
+            }
+            else{
+                ancoredObject.position.copy(intersect.point);
+            }
+
+            ancoredObject.position.y = ancoredObject.geometry.parameters.height / 2 ;
+        
+            // Verifica collisioni con altri oggetti
+            if (checkCollision()) {
+                ancoredObject.material.color.setHex(ancorObjectCollisionColor);
+            } else {
+                ancoredObject.material.color.setHex(ancorObjectColor);
+            }
+
+
+            isIntersectionFound = true;
+            return isIntersectionFound;
         }
     });
     return isIntersectionFound;
 }
+
 // allo spostamento del mouse:
 window.addEventListener('mousemove',function(e){
     mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
@@ -132,20 +204,23 @@ document.addEventListener('mousedown', function(event) {
 let intersects;
 window.addEventListener('mousedown',function(){
         if(isScaffaleDaPosizionare){
-            raycaster.setFromCamera(mousePosition,camera);
-            intersects = raycaster.intersectObjects(scene.children);
-            intersects.forEach(function(intersect){
-                if(intersect.object.name === 'ground'){
-                    const nuovoBlocco = ancoredObject.clone();
-                    scene.add(nuovoBlocco);
-                    removeMouseAnchor();                }
-            });
+            if(!checkCollision()){
+                raycaster.setFromCamera(mousePosition,camera);
+                intersects = raycaster.intersectObjects(scene.children);
+                intersects.forEach(function(intersect){
+                    if(intersect.object.name === 'ground'){
+                        const nuovoBlocco = ancoredObject.clone();
+                        scene.add(nuovoBlocco);
+                        removeMouseAnchor();                
+                    }
+                });
+            }
         }
 });
 
 
-function animate(time) {
-    renderer.render( scene, camera );
+function animate() {
+    renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
