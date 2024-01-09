4aefcb513aa5d0583ae3cd6b182ca2812c6f4345
Antonio <oselieroantonio@gmail.com>
2024-01-09 13:59:16 +0100

    react con esempio di three.js

---
+++ cra-threejs/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import Three from "./Three";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Three />
+    </div>
+  );
+}
+
+export default App;
